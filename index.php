24509bd08472ccadcb6a9d4d758b5563b12d5f0b
kota machino <65933978+airraid714@users.noreply.github.com>
2022-06-14 16:20:13 +0900

    サイドバー部分のテンプレート化

--- index.php
+++ index.php
@@ -1,44 +1,6 @@
 <?php get_header(); ?>
 
-            <aside class="l-sidebar p-sidebar c-grid__sidebar">
-                <div class="p-sidebar__inner">
-                    <div class="c-inner__button--close">
-                        <button class="c-button__close js-sideclose">
-                        </button>
-                    </div>
-                    <div class="p-menu">
-                        <ul class="p-menu__list">
-                            <h3 class="p-menu__tittle">バーガー</h3>
-
-                            <li class="p-menu__item">ハンバーガー</li>
-                            <li class="p-menu__item">チーズバーガー</li>
-                            <li class="p-menu__item">テリヤキバーガー</li>
-                            <li class="p-menu__item">アボカドバーガー</li>
-                            <li class="p-menu__item">フィッシュバーガー</li>
-                            <li class="p-menu__item">ベーコンバーガー</li>
-                            <li class="p-menu__item">チキンバーガー</li>
-                        </ul>
-                        <ul class="p-menu__list">
-                            <h3 class="p-menu__tittle">サイド</h3>
-
-                            <li class="p-menu__item">ポテト</li>
-                            <li class="p-menu__item">サラダ</li>
-                            <li class="p-menu__item">ナゲット</li>
-                            <li class="p-menu__item">コーン</li>
-                        </ul>
-                        <ul class="p-menu__list">
-                            <h3 class="p-menu__tittle">ドリンク</h3>
-
-                            <li class="p-menu__item">コーラ</li>
-                            <li class="p-menu__item">ファンタ</li>
-                            <li class="p-menu__item">オレンジ</li>
-                            <li class="p-menu__item">アップル</li>
-                            <li class="p-menu__item">紅茶（Ice/Hot）</li>
-                            <li class="p-menu__item">コーヒー（Ice/Hot）</li>
-                        </ul>
-                    </div>
-                </div>
-            </aside>
+            <?php get_sidebar(); ?>
 
             <main class="l-main p-main c-grid__main">
                 <div class="p-main__mainvisual">
@@ -275,5 +237,5 @@
                     </div>
                 </div>
             </main>
-            
+
 <?php get_footer(); ?>
---
+++ sidebar.php
@@ -0,0 +1,39 @@
+<aside class="l-sidebar p-sidebar c-grid__sidebar">
+                <div class="p-sidebar__inner">
+                    <div class="c-inner__button--close">
+                        <button class="c-button__close js-sideclose">
+                        </button>
+                    </div>
+                    <div class="p-menu">
+                        <ul class="p-menu__list">
+                            <h3 class="p-menu__tittle">バーガー</h3>
+
+                            <li class="p-menu__item">ハンバーガー</li>
+                            <li class="p-menu__item">チーズバーガー</li>
+                            <li class="p-menu__item">テリヤキバーガー</li>
+                            <li class="p-menu__item">アボカドバーガー</li>
+                            <li class="p-menu__item">フィッシュバーガー</li>
+                            <li class="p-menu__item">ベーコンバーガー</li>
+                            <li class="p-menu__item">チキンバーガー</li>
+                        </ul>
+                        <ul class="p-menu__list">
+                            <h3 class="p-menu__tittle">サイド</h3>
+
+                            <li class="p-menu__item">ポテト</li>
+                            <li class="p-menu__item">サラダ</li>
+                            <li class="p-menu__item">ナゲット</li>
+                            <li class="p-menu__item">コーン</li>
+                        </ul>
+                        <ul class="p-menu__list">
+                            <h3 class="p-menu__tittle">ドリンク</h3>
+
+                            <li class="p-menu__item">コーラ</li>
+                            <li class="p-menu__item">ファンタ</li>
+                            <li class="p-menu__item">オレンジ</li>
+                            <li class="p-menu__item">アップル</li>
+                            <li class="p-menu__item">紅茶（Ice/Hot）</li>
+                            <li class="p-menu__item">コーヒー（Ice/Hot）</li>
+                        </ul>
+                    </div>
+                </div>
+            </aside>
