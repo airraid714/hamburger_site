042393087d6f33255509b39a61bafb534f2212e7
kota machino <65933978+airraid714@users.noreply.github.com>
2022-06-28 11:39:18 +0900

    wpクラスのスタイリング調整

--- archive.php
+++ archive.php
@@ -3,26 +3,25 @@
 <?php get_sidebar(); ?>
 
 <main class="l-main c-grid__main">
-    <div class="p-page__wrapper">
-        <div class="p-page__inner-archive ">
-            <div class="c-layer__img"></div>
-            <?php       
-                $category = get_the_category();
-                $category = $category[0];
-            ?>
-            <?php if (function_exists('z_taxonomy_image')) z_taxonomy_image(); ?>           
-            <div class="p-page__content">
-                <h2 class="p-page__title c-title__mainvisual">Menu</h2> 
-                <p class="c-title__item u-margin-left20">
-                    <?php echo $category->name; ?>
-                </p>
-            </div>
-        </div>
-        <div class="p-articles">
-            <h2 class="c-title__articles-h2 u-margin__archive-title"><?php echo $category->name; ?></h2>
-            <p class="c-text__articles"><?php echo $category->description; ?></p>            
+    <div class="p-page__inner-archive ">
+        <div class="c-layer__img"></div>
+        <?php       
+            $category = get_the_category();
+            $category = $category[0];
+        ?>
+        <?php if (function_exists('z_taxonomy_image')) z_taxonomy_image(); ?>           
+        <div class="p-page__content">
+            <h2 class="p-page__title c-title__mainvisual">Menu</h2> 
+            <p class="c-title__item u-margin-left20">
+                <?php echo $category->name; ?>
+            </p>
         </div>
     </div>
+    <div class="p-articles">
+        <h2 class="c-title__articles-h2 u-margin__archive-title"><?php echo $category->name; ?></h2>
+        <p class="c-text__articles"><?php echo $category->description; ?></p>            
+    </div>
+
     <div class="p-item__wrapper">
         <?php if(have_posts()): ?>
         <?php while(have_posts()):
---
+++ page.php
@@ -0,0 +1,26 @@
+<?php get_header(); ?>
+<?php get_sidebar(); ?>
+
+<main class="l-main c-grid__main">
+    <?php if(have_posts()): ?>
+    <?php while(have_posts()):
+        the_post(); ?>
+            <div class="p-page__inner-archive ">
+                <div class="c-layer__img"></div>
+                <?php the_post_thumbnail(); ?>
+                <div class="p-page__content">
+                    <h2 class="c-title__mainvisual"><?php the_title(); ?></h2> 
+                </div>
+            </div>
+
+            <div class="p-page__wrapper">
+
+                <?php the_content(); ?>
+            </div>
+    <?php endwhile; ?> 
+        <?php wp_reset_postdata(); ?>
+    <?php else: ?>
+        <p>表示できるページがありません</p>
+    <?php endif; ?>                
+</main>
+<?php get_footer(); ?>
